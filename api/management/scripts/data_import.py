4b057537e71a2216073a46623b466c06dc98adc1
Concubidated <tbrekke@gmail.com>
2025-03-14 05:55:59 +0000

    handle song creations error

--- api/management/scripts/data_import.py
+++ api/management/scripts/data_import.py
@@ -9,13 +9,16 @@ def save_song_chart_data(pack, data):
 
     for parsed in data:
         # Get or create the Song entry
-        song, created = Song.objects.get_or_create(
-            pack=pack,
-            title=parsed.title,
-            artist=parsed.artist,
-            banner=parsed.banner,
-            defaults={"songlength": parsed.songlength, "banner": parsed.banner, "bpms": parsed.bpms}
-        )
+        try:
+            song, created = Song.objects.get_or_create(
+                pack=pack,
+                title=parsed.title[:255],
+                artist=parsed.artist[:255],
+                banner=parsed.banner,
+                defaults={"songlength": parsed.songlength, "banner": parsed.banner, "bpms": parsed.bpms}
+            )
+        except:
+            print("Error: Failed to save song", parsed.title)
 
         if created:
             saved_songs += 1  # Track new songs added
