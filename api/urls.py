a411a91530413dc9a3ee8918a0792f9023e36442
Concubidated <tbrekke@gmail.com>
2025-04-18 02:05:53 +0000

    csv pack list

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.hello_api, name='hello_api'),
+    path('packs', views.pack_list),
 ]
--- api/views.py
+++ api/views.py
@@ -1,7 +1,26 @@
 """Api view"""
 
-from django.http import JsonResponse
+import re
+from django.http import JsonResponse, HttpResponse
+from django.db.models import Prefetch, Min, Max, Count, F, Value
 
-def hello_api():
-    """Hello world"""
-    return JsonResponse({'message': 'Hello from the API!'})
+from api.models import Pack, Song, Chart, ChartData
+
+
+def natural_sort_key(text):
+    """order numbers naturally cause we are hoomans"""
+    return [int(part) if part.isdigit() else part.lower()
+            for part in re.split(r'(\d+)', text)]
+
+def pack_list(request):
+    """api call to list packs"""
+    packs = list(Pack.objects
+                .annotate(song_count=Count('songs')).order_by("name"))
+    packs.sort(key=lambda pack: natural_sort_key(pack.name))
+
+    out = "Pack Name, Song Count, Size\n"
+    for pack in packs:
+        out += f"{pack.name}, {pack.song_count}, {pack.size}"
+        out += "\n"
+    return HttpResponse(f"<pre>{out}</pre>")
+
