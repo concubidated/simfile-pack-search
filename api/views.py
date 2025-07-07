2c706151ee79f175be16c9f36677fa3e94194803
Concubidated <tbrekke@gmail.com>
2025-07-07 18:41:19 +0000

    api/packs add packid

--- api/views.py
+++ api/views.py
@@ -19,9 +19,9 @@ def pack_list(request):
                 .annotate(song_count=Count('songs')).order_by("name"))
     packs.sort(key=lambda pack: natural_sort_key(pack.name))
 
-    out = "Pack Name, Song Count, Size\n"
+    out = "ID, Pack Name, Song Count, Size\n"
     for pack in packs:
-        out += f"\"{pack.name}\", {pack.song_count}, {pack.size}"
+        out += f"{pack.id}, \"{pack.name}\", {pack.song_count}, {pack.size}"
         out += "\n"
     return HttpResponse(f"<pre>{out}</pre>")
 
